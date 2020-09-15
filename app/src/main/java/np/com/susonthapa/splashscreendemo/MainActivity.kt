ea362499872c87b64d8e2062bdfaddc480a693b9
suson <susuthapa19961227@gmail.com>
2020-09-15 16:32:18 +0545

    initial commit

---
+++ app/src/main/java/np/com/susonthapa/splashscreendemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package np.com.susonthapa.splashscreendemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
