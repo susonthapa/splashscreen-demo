b8aa946d9aa45ac4b7fc81ccc82db0fded45f6a2
suson <susuthapa19961227@gmail.com>
2020-09-15 16:52:20 +0545

    basic activity implemented with no splash screen and heavy layout inflation simulation

--- app/src/main/java/np/com/susonthapa/splashscreendemo/MainActivity.kt
+++ app/src/main/java/np/com/susonthapa/splashscreendemo/MainActivity.kt
@@ -2,10 +2,22 @@ package np.com.susonthapa.splashscreendemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.math.atan
+import kotlin.math.tan
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        simulateHeavyLayout()
         setContentView(R.layout.activity_main)
     }
+
+    private fun simulateHeavyLayout() {
+        // simulate some heavy UI inflation
+        for (i in 0..1000000) {
+            val d = tan(atan(tan(atan(tan(atan(tan(atan(tan(atan(123456789.123456789))))))))))
+            d.toString()
+        }
+    }
+
 }
