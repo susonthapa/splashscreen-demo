d27f2313225f0729fa94c1bd4b6579fb161d11bc
suson <susuthapa19961227@gmail.com>
2020-09-15 17:12:23 +0545

    basic splash screen implemented

---
+++ app/src/main/java/np/com/susonthapa/splashscreendemo/SplashActivity.kt
@@ -0,0 +1,19 @@
+package np.com.susonthapa.splashscreendemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
