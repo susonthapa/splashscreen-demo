78fe56884cf021ce30f64cc8faf0d106ed0bc0eb
suson <susuthapa19961227@gmail.com>
2020-09-18 04:58:18 +0545

    animations added to vector icons, issues with api below 21 fixed

--- app/src/main/java/np/com/susonthapa/splashscreendemo/SplashActivity.kt
+++ app/src/main/java/np/com/susonthapa/splashscreendemo/SplashActivity.kt
@@ -1,6 +1,8 @@
 package np.com.susonthapa.splashscreendemo
 
 import android.content.Intent
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -13,6 +15,25 @@ import androidx.constraintlayout.widget.ConstraintLayout
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val view = layoutInflater.inflate(R.layout.activity_splash, null, false)
+        // we want to keep the logo of the app centered and at the same position the theme sets the logo to.
+        // since the theme positions the logo with respect to the entire window, we need to add top margin
+        // to our image view which should be equal to the height of the status bar
+        view.viewTreeObserver.addOnPreDrawListener(object: ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                view.viewTreeObserver.removeOnPreDrawListener(this)
+                // set the margin to the image
+                val imageView = view.findViewById<ImageView>(R.id.splash_image)
+                val layoutParams = imageView.layoutParams as ConstraintLayout.LayoutParams
+                val statusBarHeight = resources.displayMetrics.heightPixels - view.measuredHeight
+                Log.d("height:", statusBarHeight.toString())
+                layoutParams.setMargins(0, statusBarHeight, 0, 0)
+                imageView.layoutParams = layoutParams
+                (imageView.drawable as Animatable).start()
+                return true
+            }
+        })
+        setContentView(view)
         // simulate some async initialization like authenticating user from network
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
